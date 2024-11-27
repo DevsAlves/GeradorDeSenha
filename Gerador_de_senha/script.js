ba1f503d65063b0cc6531bacbc43fc718c55ce87
Guilherme_Alves <Alves@gmail.com>
2024-11-26 23:36:30 -0300

    Setup inicial

---
+++ Gerador_de_senha/script.js
@@ -0,0 +1,14 @@
+const letter = document.getElementsByClassName("letters");
+const size = document.getElementsByClassName("size"); 
+const submit = document.getElementsByClassName("submit");
+
+letter.addEventListener("click", values => {
+    const valueLetter = letter.value.toLowerCase();
+    function Adicionar () {
+    
+    };
+})
+
+
+
+// OBS : tentar não usar o chat gpt 
