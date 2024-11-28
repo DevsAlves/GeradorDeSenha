5c11d5b9e1880a10f9d49d81c4c4315137fa0b33
Guilherm Alves <you@example.com>
2024-11-27 21:19:57 -0300

    alter

--- Gerador_de_senha/outro.js
+++ Gerador_de_senha/outro.js
@@ -1,21 +1,28 @@
-const letter = document.querySelector(".letters");
+const letras = document.querySelector(".letras"); 
 const size = document.querySelector(".size"); 
 const submit = document.getElementById("submit");
 
 
 submit.addEventListener("click", event => { 
-    event.preventDefault(); 
+    event.preventDefault()
 
-    if(letter.value.trim() !== "") {
-        const valueLetter = letter.value.toLowerCase().trim();
+    if(letras.value.trim() !== "") {
+        const valueLetter = letras.value.toLowerCase().trim();
 
         function add() { 
             const arrayValue = valueLetter.split(",").map(item => item.trim()).filter(item => item !== "");
             console.log(arrayValue); 
         }
+        
+        
+
+
         add();
     } else {
         console.error("ERRO"); 
         alert("Por favor preencha os campos corretamente");
     }
 });
+
+
+// O que faz o trim() -> remove os espaços brancos de uma string 
