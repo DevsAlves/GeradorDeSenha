583812c40cfa2152f595b661e2b5ae20c717c5cc
Guilherme_Alves <Alves@gmail.com>
2024-11-27 22:21:25 -0300

    code em andamento

--- Gerador_de_senha/outro.js
+++ Gerador_de_senha/outro.js
@@ -7,16 +7,21 @@ submit.addEventListener("click", event => {
     event.preventDefault()
 
     if(letras.value.trim() !== "") {
-        const valueLetter = letras.value.toLowerCase().trim();
+        const valueLetter = letras.value;
 
         function add() { 
             const arrayValue = valueLetter.split(",").map(item => item.trim()).filter(item => item !== "");
             console.log(arrayValue); 
-        }
-        
-        
 
+            const result = function embaralharArray() {
+                    arrayValue.sort((elem1,elem2)=> Math.random() - Math.random());
+                };
 
+                embaralharArray(); 
+
+                const resultArray = result.join(",").replace(/\s+/g, ""); 
+                console.log(resultArray);  
+        };
         add();
     } else {
         console.error("ERRO"); 
