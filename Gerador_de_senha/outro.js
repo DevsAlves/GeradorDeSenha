50037e532eda87eacbdebcccbbe86dc4776def68
Guilherme_Alves <Alves@gmail.com>
2024-11-28 23:43:29 -0300

    Setup + code

--- Gerador_de_senha/outro.js
+++ Gerador_de_senha/outro.js
@@ -3,25 +3,36 @@ const size = document.querySelector(".size");
 const submit = document.getElementById("submit");
 
 
+
 submit.addEventListener("click", event => { 
     event.preventDefault()
 
     if(letras.value.trim() !== "") {
         const valueLetter = letras.value;
+        // const valueSize = parseInt(size.value);
 
         function add() { 
             const arrayValue = valueLetter.split(",").map(item => item.trim()).filter(item => item !== "");
             console.log(arrayValue); 
 
-            const result = function embaralharArray() {
-                    arrayValue.sort((elem1,elem2)=> Math.random() - Math.random());
-                };
 
-                embaralharArray(); 
 
+            // Corrigir essa função pois está dando errado
+            function embaralharArray(array) {
+                let oldElement;
+                for (let i = array.length - 1; i > 0; i--) {
+                    let rand = Math.floor(Math.random() * (i + 1));
+                    oldElement = array[i];
+                    array[i] = array[rand];
+                    array[rand] = oldElement;
+                }
+                    return array;
+            };
+                const result =  embaralharArray(arrayValue);   
                 const resultArray = result.join(",").replace(/\s+/g, ""); 
                 console.log(resultArray);  
         };
+
         add();
     } else {
         console.error("ERRO"); 
@@ -30,4 +41,4 @@ submit.addEventListener("click", event => {
 });
 
 
-// O que faz o trim() -> remove os espaços brancos de uma string 
+// O que faz o trim() -> remove os espaços brancos no ínicio e no fim  de uma string 
